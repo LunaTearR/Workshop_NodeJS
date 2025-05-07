59759c96dbc9ef24b2d5fea959837f38f9b34934
saengtawan <saengtawan@bananacoding.com>
2025-05-07 17:34:52 +0700

    login & register user

---
+++ routes/api/v1/login.js
@@ -0,0 +1,41 @@
+var express = require("express");
+var router = express.Router();
+var userSchema = require("../../../models/user.model");
+const bcrypt = require("bcrypt");
+
+router.post("/", async function (req, res, next) {
+  try {
+    let { email, password } = req.body;
+
+    const user = await userSchema.findOne({ email });
+
+    if (!user) {
+      return res
+        .status(404)
+        .json({ status: 404, message: "User not found. Please register." });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res
+        .status(401)
+        .json({ status: 401, message: "Incorrect password." });
+    }
+
+    const isStatusApproved = user.status;
+
+    if (isStatusApproved !== "approve") {
+      return res.status(403).json({
+        status: 401,
+        message: "User not approved. Please contact admin.",
+      });
+    }
+
+    res.send({ message: "Login successful", user });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", error });
+  }
+});
+
+module.exports = router;
